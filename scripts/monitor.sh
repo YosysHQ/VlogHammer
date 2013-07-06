5ba57ecfa06e7866c8aa66b58a1c6bead2214c9d
Clifford Wolf <clifford@clifford.at>
2013-07-06 02:51:22 +0200

    Improved monitor script

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-date
 uptime
+echo
+
+sum=0
+for dir in rtl {syn,check}_{vivado,quartus,xst,yosys} report; do
+	test -d $dir || continue
+	count=$( ls $dir | wc -l )
+	printf "%-20s%5s\n" $dir $count
+	sum=$((sum+count))
+done
+
+date "+$sum   [%H:%M:%S]" | figlet -W -w160
+statuslen=$((sum % 20 + (sum % 20 - 1) / 5))
+[ $statuslen -eq 0 ] && statuslen=160
+printf "%.*s" $statuslen "XXXXX,XXXXX,XXXXX,XXXXX" | figlet -fsmall -w160
 
 echo
 for pid in $( pidof make ); do
-	pstree -p $pid
-done
-
-echo
-for dir in rtl {syn,check}_{vivado,quartus,xst,yosys} report; do
-	test -d $dir || continue
-	printf "%-20s%5s\n" $dir $( ls $dir | wc -l )
+	pstree $pid
 done
 
