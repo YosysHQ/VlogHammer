208276ea5f6fb86424efe1557dcee713bc42978d
Clifford Wolf <clifford@clifford.at>
2013-07-10 00:43:57 +0200

    Improved monitor script

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -21,71 +21,83 @@
 #	watch -n20 bash scripts/monitor.sh
 #
 
-sum=0
-function stat_line()
 {
-	for dir; do
-		printf "%5s|%5s" "" ""
-		if [ "$dir" = "" ]; then
-			printf "%-15s%5s" "" ""
-			continue
-		fi
-		if ! test -d $dir && [ "$dir" != "total" -a "$dir" != "failed" ]; then
-			printf "%-15s%5s" "$dir" "----"
-			continue
-		fi
-		if [ $dir == "failed" ]; then
-			count=$( ls check_vivado check_quartus check_xst check_yosys 2> /dev/null | grep '\.err$' | sort -u | wc -l )
-		elif [ $dir == "total" ]; then
-			count=$sum
-		else
-			count=$( ls $dir | grep -v '\.err$' | wc -l )
-		fi
-		printf "%-15s%5s" $dir $count
-		if [ $dir != "total" -a $dir != "failed" ]; then
-			sum=$((sum+count))
-		fi
+	sum=0
+	function stat_line()
+	{
+		for dir; do
+			printf "%5s|%5s" "" ""
+			if [ "$dir" = "" ]; then
+				printf "%-15s%5s" "" ""
+				continue
+			fi
+			if ! test -d $dir && [ "$dir" != "total" -a "$dir" != "failed" ]; then
+				printf "%-15s%5s" "$dir" "----"
+				continue
+			fi
+			if [ $dir == "failed" ]; then
+				count=$( ls check_vivado check_quartus check_xst check_yosys 2> /dev/null | grep '\.err$' | sort -u | wc -l )
+			elif [ $dir == "total" ]; then
+				count=$sum
+			else
+				count=$( ls $dir | grep -v '\.err$' | wc -l )
+			fi
+			printf "%-15s%5s" $dir $count
+			if [ $dir != "total" -a $dir != "failed" ]; then
+				sum=$((sum+count))
+			fi
+		done
+		printf "%5s|\n" ""
+	}
+	echo "     +------------------------------+------------------------------+------------------------------+"
+	stat_line {syn,check}_vivado   rtl
+	stat_line {syn,check}_quartus  failed
+	stat_line {syn,check}_xst      report
+	stat_line {syn,check}_yosys    total
+	echo "     +------------------------------+------------------------------+------------------------------+"
+
+	mkdir -p ~/.vloghammer
+	{ tail -n100 ~/.vloghammer/monitordata.txt 2> /dev/null;
+			date "+%s $sum"; } > ~/.vloghammer/monitordata.new
+	mv ~/.vloghammer/monitordata.new ~/.vloghammer/monitordata.txt
+
+	echo
+	echo -n "$(uptime),  floating avg. sec/job: "
+	gawk 'ARGIND == 1 { mintm = $1; maxtm = $1; mincnt = $2; maxcnt = $2; }
+	      ARGIND == 2 && $1 > maxtm-600 && $1 < mintm { mintm = $1; mincnt = $2; }
+	      END { printf "%.2f\n", (maxtm - mintm) / (maxcnt - mincnt + 1); }' \
+	      ~/.vloghammer/monitordata.txt ~/.vloghammer/monitordata.txt
+
+	echo
+
+	statuslen=$((sum % 50))
+	if [ $statuslen -gt 25 ]; then
+		statusbar="$( printf "%.*slllllllllllllllllllllllll" $((statuslen-25)) "........................." )"
+	else
+		statusbar="$( printf "%.*s........................." $statuslen "lllllllllllllllllllllllll" )"
+	fi
+	statusbar="$( echo "$statusbar" | sed -r '
+		s/^(.{4})\./\1,/; s/^(.{4})l/\1|/;
+		s/^(.{9})\./\1,/; s/^(.{9})l/\1|/;
+		s/^(.{14})\./\1,/; s/^(.{14})l/\1|/;
+		s/^(.{19})\./\1,/; s/^(.{19})l/\1|/;
+		s/^(.{24})\./\1,/; s/^(.{24})l/\1|/;
+	' )"
+	printf "   %.25s     $(echo $sum | sed 's/./\0 /g;')" "$statusbar" | figlet -w160
+
+	echo
+	for pid in $( ps h -o pid,ppid $( pidof make ) | gawk '{ d[$1]=$2; } END { for (p in d) if (!(d[p] in d)) print p; }' ); do
+		pstree $pid
 	done
-	printf "%5s|\n" ""
-}
-echo "     +------------------------------+------------------------------+------------------------------+"
-stat_line {syn,check}_vivado   rtl
-stat_line {syn,check}_quartus  failed
-stat_line {syn,check}_xst      report
-stat_line {syn,check}_yosys    total
-echo "     +------------------------------+------------------------------+------------------------------+"
+} | tee monitor.txt_new
 
-mkdir -p ~/.vloghammer
-{ tail -n100 ~/.vloghammer/monitordata.txt 2> /dev/null;
-		date "+%s $sum"; } > ~/.vloghammer/monitordata.new
-mv ~/.vloghammer/monitordata.new ~/.vloghammer/monitordata.txt
+{
+	echo '<html>'
+	echo '<meta http-equiv="refresh" content="10"/>'
+	echo "<pre>$( perl -pe 's/([<>&])/"&#".ord($1).";"/eg;' < monitor.txt )</pre>"
+	echo '</html>'
+} > monitor.html_new
 
-echo
-echo -n "$(uptime),  floating avg. sec/job: "
-gawk 'ARGIND == 1 { mintm = $1; maxtm = $1; mincnt = $2; maxcnt = $2; }
-      ARGIND == 2 && $1 > maxtm-600 && $1 < mintm { mintm = $1; mincnt = $2; }
-      END { printf "%.2f\n", (maxtm - mintm) / (maxcnt - mincnt + 1); }' \
-      ~/.vloghammer/monitordata.txt ~/.vloghammer/monitordata.txt
-
-echo
-
-statuslen=$((sum % 50))
-if [ $statuslen -gt 25 ]; then
-	statusbar="$( printf "%.*slllllllllllllllllllllllll" $((statuslen-25)) "........................." )"
-else
-	statusbar="$( printf "%.*s........................." $statuslen "lllllllllllllllllllllllll" )"
-fi
-statusbar="$( echo "$statusbar" | sed -r '
-	s/^(.{4})\./\1,/; s/^(.{4})l/\1|/;
-	s/^(.{9})\./\1,/; s/^(.{9})l/\1|/;
-	s/^(.{14})\./\1,/; s/^(.{14})l/\1|/;
-	s/^(.{19})\./\1,/; s/^(.{19})l/\1|/;
-	s/^(.{24})\./\1,/; s/^(.{24})l/\1|/;
-' )"
-printf "   %.25s     $(echo $sum | sed 's/./\0 /g;')" "$statusbar" | figlet -w160
-
-echo
-for pid in $( ps h -o pid,ppid $( pidof make ) | gawk '{ d[$1]=$2; } END { for (p in d) if (!(d[p] in d)) print p; }' ); do
-	pstree $pid
-done
+mv monitor.txt_new monitor.txt
+mv monitor.html_new monitor.html
 
