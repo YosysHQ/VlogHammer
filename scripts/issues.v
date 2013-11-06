cd659d1011e6adf676cbea53254591083ba0af63
Clifford Wolf <clifford@clifford.at>
2013-11-06 02:14:47 +0100

    Fixed typo in issue_002 comment

--- scripts/issues.v
+++ scripts/issues.v
@@ -23,7 +23,7 @@ module issue_002(a, b, y);
   output [0:0] y;
   // the width of $signed(a) is self-determined. so it must return a 2-bit
   // value that is then zero-extended to three bits because the comparison
-  // with b is done in an unsinged context (b is unsigned).
+  // with b is done in an unsigned context (b is unsigned).
   assign y = $signed(a) == b;
 endmodule
 module issue_003(a, y);
