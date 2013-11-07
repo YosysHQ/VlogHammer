d5efa402756ea7a5eb164268b4293f35ef567faa
Clifford Wolf <clifford@clifford.at>
2013-11-07 16:59:57 +0100

    Added modelsim.sh utility script

---
+++ scripts/modelsim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# simple helper script to run stuff with modelsim
+# (useful during troubleshooting)
+#
+# Usage example:
+#
+#  1.) add an initial-block with $display statements to rtl/test.v
+#  2,) run "bash scripts/modelsim.sh test"
+#
+
+MODELSIM_DIR=$( grep ^MODELSIM_DIR Makefile | cut -f2 -d= )
+$MODELSIM_DIR/vlib work
+$MODELSIM_DIR/vlog rtl/$1.v
+$MODELSIM_DIR/vsim -c -do "run; exit" work.$1
+rm -rf work transcript
+
