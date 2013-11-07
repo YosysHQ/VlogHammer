0668be793abdbc0dfe30c668d898dfd9082fd8bb
Clifford Wolf <clifford@clifford.at>
2013-11-07 19:17:20 +0100

    Added more archived issues

--- scripts/issues.v
+++ scripts/issues.v
@@ -63,3 +63,29 @@ module issue_005(a, y);
   assign y6 = |(a/a);
   assign y7 = (|a)/(|a);
 endmodule
+module issue_006(a, y);
+  input signed [1:0] a;
+  wire [0:0] y0;
+  wire [0:0] y1;
+  output [1:0] y;
+  assign y ={y0,y1};
+  // the 1'bx must be extended to 2'b0x and not 2'bxx
+  assign y0 = a != 1'bx;
+  assign y1 = a == 1'bx;
+endmodule
+module issue_007(a, y);
+  input [3:0] a;
+  wire [3:0] y0;
+  wire [3:0] y1;
+  wire [3:0] y2;
+  wire [3:0] y3;
+  output [15:0] y;
+  assign y = {y0,y1,y2,y3};
+  // constant evaluation of width-extension with undefs
+  localparam [1:0] p0 = |(1/0);
+  localparam [1:0] p1 = (|1)/(|0);
+  assign y0 = p0;         // 4'b000x
+  assign y1 = p1;         // 4'b00xx
+  assign y2 = |(1/0);     // 4'b000x
+  assign y3 = (|1)/(|0);  // 4'bxxxx
+endmodule
