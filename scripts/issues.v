0bc6fedc8a2839cfc3bf1c3d984a58291bd9a2fa
Clifford Wolf <clifford@clifford.at>
2013-12-29 21:28:36 +0100

    Added two more issues

--- scripts/issues.v
+++ scripts/issues.v
@@ -200,3 +200,81 @@ module issue_008(a, y);
   assign y14 = a == 0 ? ss14 : a == 1 ? su14 : a == 2 ? us14 : uu14;
   assign y15 = a == 0 ? ss15 : a == 1 ? su15 : a == 2 ? us15 : uu15;
 endmodule
+module issue_009(a, y);
+  input [3:0] a;
+  output [67:0] y;
+
+  wire [3:0] y0;
+  wire [3:0] y1;
+  wire [3:0] y2;
+  wire [3:0] y3;
+  wire [3:0] y4;
+  wire [3:0] y5;
+  wire [3:0] y6;
+  wire [3:0] y7;
+  wire [3:0] y8;
+  wire [3:0] y9;
+  wire [3:0] y10;
+  wire [3:0] y11;
+  wire [3:0] y12;
+  wire [3:0] y13;
+  wire [3:0] y14;
+  wire [3:0] y15;
+  wire [3:0] y16;
+  wire [3:0] y17;
+  wire [3:0] y18;
+
+  assign y = {y0,y1,y2,y3,y4,y5,y6,y7,y8,y9,y10,y11,y12,y13,y14,y15,y16,y17,y18};
+
+  // bitwise operations must 0-extend their arguments (even with undef msb)
+  // (the arithmetic operations don't, but it does not matter because
+  // a single undef bit in the argument will cause the result to be undef)
+  assign y0  = a +  1'bx;
+  assign y1  = a -  1'bx;
+  assign y2  = a *  1'bx;
+  assign y3  = a /  1'bx;
+  assign y4  = a %  1'bx;
+  assign y5  = a >  1'bx;
+  assign y6  = a >= 1'bx;
+  assign y7  = a <  1'bx;
+  assign y8  = a <= 1'bx;
+  assign y9  = a && 1'bx;
+  assign y10 = a || 1'bx;
+  assign y11 = a == 1'bx;
+  assign y12 = a != 1'bx;
+  assign y13 = a &  1'bx;
+  assign y14 = a |  1'bx;
+  assign y15 = a ^  1'bx;
+  assign y16 = a ^~ 1'bx;
+  assign y17 = + 1'bx;
+  assign y18 = - 1'bx;
+endmodule
+module issue_010(a, b, y);
+  input [5:0] a;
+  input [3:0] b;
+
+  // I have no clue why but Vivado 2013.3 generates a netlist containing:
+  //
+  //   assign \<const0>  = a[3];
+  //   assign \<const0>  = a[2];
+  //   assign \<const0>  = a[1];
+  //   assign \<const0>  = b[3];
+  //   assign \<const0>  = b[2];
+  //   assign \<const0>  = b[1];
+  //   assign \<const0>  = b[0];
+  //
+  //   IBUF IBUF
+  //         (.I(\<const0> ),
+  //          .O(xlnx_opt_));
+
+  wire [80:0] y0;
+  wire [4:0] y1;
+  wire [3:0] y2;
+
+  output [89:0] y;
+  assign y = {y0,y1,y2};
+
+  assign y0  = 0;
+  assign y1  = {4{{3{b}}}};
+  assign y2  = 4'b1000 * a;
+endmodule
