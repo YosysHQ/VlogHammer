9fa0516a90ac5951271cffdf2f939e6df295f7ba
Clifford Wolf <clifford@clifford.at>
2013-12-30 11:20:51 +0100

    Updated scripts/issues.v

--- scripts/issues.v
+++ scripts/issues.v
@@ -63,15 +63,50 @@ module issue_005(a, y);
   assign y6 = |(a/a);
   assign y7 = (|a)/(|a);
 endmodule
-module issue_006(a, y);
-  input signed [1:0] a;
-  wire [0:0] y0;
-  wire [0:0] y1;
-  output [1:0] y;
-  assign y ={y0,y1};
-  // the 1'bx must be extended to 2'b0x and not 2'bxx
-  assign y0 = a != 1'bx;
-  assign y1 = a == 1'bx;
+module issue_006(y);
+  output [4:0] y;
+  // icarus verilog has a bug (git 336b299) that prevents it from
+  // evaluating ^~ in parameters. This is just a quick test of all
+  // verilog expressions in localparams to catch such bugs.
+  localparam [4:0] pb00 = 5'd00 +   5'd0;
+  localparam [4:0] pb01 = 5'd01 -   5'd0;
+  localparam [4:0] pb02 = 5'd02 *   5'd0;
+  localparam [4:0] pb03 = 5'd03 /   5'd0;
+  localparam [4:0] pb04 = 5'd04 %   5'd0;
+  localparam [4:0] pb05 = 5'd05 **  5'd0;
+  localparam [4:0] pb06 = 5'd06 >   5'd0;
+  localparam [4:0] pb07 = 5'd07 >=  5'd0;
+  localparam [4:0] pb08 = 5'd08 <   5'd0;
+  localparam [4:0] pb09 = 5'd09 <=  5'd0;
+  localparam [4:0] pb10 = 5'd10 &&  5'd0;
+  localparam [4:0] pb11 = 5'd11 ||  5'd0;
+  localparam [4:0] pb12 = 5'd12 ==  5'd0;
+  localparam [4:0] pb13 = 5'd13 !=  5'd0;
+  localparam [4:0] pb14 = 5'd14 === 5'd0;
+  localparam [4:0] pb15 = 5'd15 !== 5'd0;
+  localparam [4:0] pb16 = 5'd16 &   5'd0;
+  localparam [4:0] pb17 = 5'd17 |   5'd0;
+  localparam [4:0] pb18 = 5'd18 ^   5'd0;
+  localparam [4:0] pb19 = 5'd19 ^~  5'd0;
+  localparam [4:0] pb20 = 5'd20 <<  5'd0;
+  localparam [4:0] pb21 = 5'd21 >>  5'd0;
+  localparam [4:0] pb22 = 5'd22 <<< 5'd0;
+  localparam [4:0] pb23 = 5'd23 >>> 5'd0;
+  localparam [4:0] pu00 = +  5'd00;
+  localparam [4:0] pu01 = -  5'd01;
+  localparam [4:0] pu02 = !  5'd02;
+  localparam [4:0] pu03 = ~  5'd03;
+  localparam [4:0] pu04 = &  5'd04;
+  localparam [4:0] pu05 = ~& 5'd05;
+  localparam [4:0] pu06 = |  5'd06;
+  localparam [4:0] pu07 = ~| 5'd07;
+  localparam [4:0] pu08 = ^  5'd08;
+  localparam [4:0] pu09 = ~^ 5'd09;
+  localparam [4:0] pter = 1 ? 2 : 3;
+  assign y = pb00 ^ pb01 ^ pb02 ^ pb03 ^ pb04 ^ pb05 ^ pb06 ^ pb07 ^ pb08 ^
+             pb09 ^ pb10 ^ pb11 ^ pb12 ^ pb13 ^ pb14 ^ pb15 ^ pb16 ^ pb17 ^
+             pb18 ^ pb19 ^ pb20 ^ pb21 ^ pb22 ^ pb23 ^ pu00 ^ pu01 ^ pu02 ^
+             pu03 ^ pu04 ^ pu05 ^ pu06 ^ pu07 ^ pu08 ^ pu09 ^ pter;
 endmodule
 module issue_007(a, y);
   input [3:0] a;
@@ -250,6 +285,7 @@ module issue_009(a, y);
   assign y18 = - 1'bx;
 endmodule
 module issue_010(a, b, y);
+  // http://forums.xilinx.com/t5/Synthesis/Vivado-creates-netlist-with-inputs-shorted-together/td-p/397161
   input [5:0] a;
   input [3:0] b;
 
@@ -266,6 +302,8 @@ module issue_010(a, b, y);
   //   IBUF IBUF
   //         (.I(\<const0> ),
   //          .O(xlnx_opt_));
+  //
+  // (when synthesized with (* use_dsp48 = "no" *) set on the module)
 
   wire [80:0] y0;
   wire [4:0] y1;
