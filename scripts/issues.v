12913c62a9b3416b5e61e3a16bdfc39dd489358c
Clifford Wolf <clifford@clifford.at>
2014-01-06 14:59:48 +0100

    Added issue_013

--- scripts/issues.v
+++ scripts/issues.v
@@ -331,3 +331,15 @@ module issue_012(a, y);
   // operator (y should be 4'bx when a is zero, but iverilog returns 4'd1).
   assign y = 4'd2 ** (4'd1/a);
 endmodule
+module issue_013(a, y);
+  input signed [3:0] a;
+  output [1:0] y;
+  // icarus verilog (git d1c9dd5) evaluates bit-wise operations of signed values
+  // to unsigned values when the arguments are constant. Thus "a = 0" yields
+  // "y[0] = 0" in icarus verilog, even though it should be "y[1] = 1 (see
+  // sec. 5.5.1 of IEEE Std 1365-2005). bitwise operations of variables or
+  // variables with constants are implemented correctly. So y[1] always shows
+  // the right value.
+  assign y[0] = a > (4'sb1010 | 4'sd0);
+  assign y[1] = (a | 4'sd0) > 4'sb1010;
+endmodule
