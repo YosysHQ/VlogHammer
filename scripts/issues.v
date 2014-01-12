faa82f3b248ab4a03ff89322e05abaf15866637e
Clifford Wolf <clifford@clifford.at>
2014-01-12 03:07:26 +0100

    Added issue_014

--- scripts/issues.v
+++ scripts/issues.v
@@ -349,3 +349,11 @@ module issue_013(a, y);
   assign y[0] = a > (4'sb1010 | 4'sd0);
   assign y[1] = (a | 4'sd0) > 4'sb1010;
 endmodule
+module issue_014(a, b, y);
+  input [1:0] a;
+  input [2:0] b;
+  output [3:0] y;
+  // Vivado 2013.4 asserts on this test case:
+  // vivado: /.../gencore/dp/GDpGenDivMod.cc:324: void GDpGen::implementDivMod(DFNode*, bool): Assertion `TBD' failed.
+  assign y = $signed(a / b);
+endmodule
