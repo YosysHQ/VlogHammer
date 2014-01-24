6d4b9116be3a696c324a343ddb91b861361cc92e
Clifford Wolf <clifford@clifford.at>
2014-01-24 02:07:05 +0100

    Added issue_018

--- scripts/issues.v
+++ scripts/issues.v
@@ -406,3 +406,13 @@ module issue_017(ctrl, y);
   assign y0 = &($signed(2'b11));
   assign y1 = &($unsigned(2'b11));
 endmodule
+module issue_018(a, y);
+  input [3:0] a;
+  output [3:0] y;
+
+  // in a localparam, like a wire, the width of the param should be
+  // use to extend the expression. so this should be equal to ~4'b0001.
+  // but isim 14.1 does not do this correctly.
+  localparam [3:0] p = ~1'b1;
+  assign y = p;
+endmodule
