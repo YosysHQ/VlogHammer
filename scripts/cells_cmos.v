a56f13a5f5f555dbfaceb86fb8b3a070a605e231
Clifford Wolf <clifford@clifford.at>
2014-02-15 13:25:56 +0100

    Fixed verilog syntax in cells_cmos.v

--- scripts/cells_cmos.v
+++ scripts/cells_cmos.v
@@ -1,17 +1,20 @@
 
 module NOT(A, Y);
 input A;
-output Y = ~A;
+output Y;
+assign Y = ~A;
 endmodule
 
 module NAND(A, B, Y);
 input A, B;
-output Y = ~(A & B);
+output Y;
+assign Y = ~(A & B);
 endmodule
 
 module NOR(A, B, Y);
 input A, B;
-output Y = ~(A | B);
+output Y;
+assign Y = ~(A | B);
 endmodule
 
 module DFF(C, D, Q);
