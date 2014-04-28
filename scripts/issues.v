10d8ba9662c33c00b5b5fd0968fd1cda485ce873
Clifford Wolf <clifford@clifford.at>
2014-04-28 17:32:09 +0200

    Bugs in DC G-2012.06-SP4

--- scripts/issues.v
+++ scripts/issues.v
@@ -629,9 +629,20 @@ module issue_036(a, b, c, d, y);
   input [3:0] b;
   input [3:0] c;
   input [3:0] d;
-  output [3:0] y;
+  output [11:0] y;
+  wire [3:0] y1;
+  wire [3:0] y2;
+  wire [3:0] y3;
 
   // This should return y=15 for a=15, b=15, c=15, d=15.
   // But Design Compiler G-2012.06-SP4 returns y=7 instead.
-  assign y = a >>> ((b == c) >>> d);
+  assign y1 = a >>> ((b == c) >>> d);
+
+  // This should return 1 but DC G-2012.06-SP4 returns 0 instead.
+  assign y2 = (|1 >>> 1) == |0;
+
+  // This should return 0 but DC G-2012.06-SP4 returns 1 instead.
+  assign y3 = {|1 >>> 1};
+
+  assign y = { y1, y2, y3 };
 endmodule
