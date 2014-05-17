b6402ddaf2ee27077b8dc16d012d80ebc90de0f3
Clifford Wolf <clifford@clifford.at>
2014-05-17 19:14:01 +0200

    Added testbench check_modelsim

---
+++ testbench/scripts/check_modelsim.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+MODELSIM_DIR=/opt/altera/13.1/modelsim_ase/bin
+
+mkdir -p check_modelsim
+rm -f check_modelsim/${1}_modelsim.txt
+
+rm -rf temp/check_modelsim_$1
+mkdir -p temp/check_modelsim_$1
+cd temp/check_modelsim_$1
+
+cat ../../spec/${1}_spec.v ../../scripts/check.v ../../rtl/$1.v > runme.v
+cp ../../refdat/${1}_refdat.txt refdat.txt
+
+{
+	$MODELSIM_DIR/vlib work
+	$MODELSIM_DIR/vlog +define+MATCH_DC runme.v
+	$MODELSIM_DIR/vsim -c -do "run -all; exit" work.check
+} > ../../check_modelsim/${1}_modelsim.txt
+
