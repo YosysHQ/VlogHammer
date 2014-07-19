7c90092f518b66ad204a0ac7e68f8261f7cab5a9
Clifford Wolf <clifford@clifford.at>
2014-07-20 01:43:25 +0200

    Added YOSYS_BIN and YOSYS_SCRIPT to testbench/scripts/check_yosys.sh

--- testbench/scripts/check_yosys.sh
+++ testbench/scripts/check_yosys.sh
@@ -9,7 +9,7 @@ rm -rf temp/check_yosys_$1
 mkdir -p temp/check_yosys_$1
 cd temp/check_yosys_$1
 
-yosys -q -l synth.log -p 'proc; opt; techmap; opt; abc;;' -o synth.v ../../rtl/$1.v
+${YOSYS_BIN:-yosys} -q -l synth.log -p "${YOSYS_SCRIPT:-proc; opt; techmap; opt; abc;;}" -o synth.v ../../rtl/$1.v
 
 cat ../../spec/${1}_spec.v ../../scripts/check.v synth.v > runme.v
 cp ../../refdat/${1}_refdat.txt refdat.txt
