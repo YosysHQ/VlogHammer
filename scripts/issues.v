dca690a22499a2d11921ce2811a16d42022e00cd
Clifford Wolf <clifford@clifford.at>
2014-09-23 11:59:31 +0200

    added issue_057

--- scripts/issues.v
+++ scripts/issues.v
@@ -852,3 +852,13 @@ module issue_056(a, y);
 
   assign y = ^{2{{|1, |1 << a}}};
 endmodule
+module issue_057(a, y);
+  input [2:0] a;
+  output [3:0] y;
+
+  // this is expected to set y[3:1] = 'b100 for a = 2.
+  // but verilator_3_864 sets y[3:1] = 'b000 instead.
+
+  localparam [5:15] p = 51681708;
+  assign y = p[15 + a -: 5];
+endmodule
